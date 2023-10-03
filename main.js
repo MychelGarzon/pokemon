ec10c72109422b51f492e6b865bbe152162de186
Mychel Garzon <mtjomy7@gmail.com>
2023-10-03 14:41:11 +0300

    JS and icons

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 const content = document.querySelector('.grid-container');
-const pokeData = [1, 2, 3, 4, 5];
+let pokeData = [1, 2, 3, 4, 5];
 
-const fetchData = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
+const fetchData = async () => {
+  await fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
     .then((response) => response.json())
-    .then((data) => console.log(data));
-
+    .then((data) => {
+      pokeData = data.results;
+      console.log(data.results);
+    });
   pokeCards();
 };
 
 const pokeCards = () => {
-  const cards = pokeData.map((pokemon) => {
-    return ` <div class="grid-item" id="grid1">
-    <div class=id><p>id</p> </div
+  const cards = pokeData
+    .map((pokemon) => {
+      return ` <div class="grid-item" id="grid1">
+    <div class=id><p>1</p> </div
           <span><img src="/media/Pikachu.png" alt="Pikachu" />
-          <h3>Name will be here</h3></span>
-          <span> 
+          <h3>${pokemon.name}</h3></span>
           <div class=types>
-          <img class="icon" id="normal" src="media/Normal.ico" /></span> 
-          <img class="icon" id="normal" src="media/Electric.ico" /></span> 
+          <img class="icon" id="normal" src="media/icons/Normal.ico" /></span> 
+          <img class="icon" id="normal" src="media/icons/Electric.ico" /></span> 
           </div>
       </div>`;
-  });
+    })
+    .join('');
 
   content.innerHTML = cards;
 };
