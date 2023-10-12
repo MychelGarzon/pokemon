5f6e19e2d8c4e9b76f49854fb1c285213b544410
Mychel Garzon <mtjomy7@gmail.com>
2023-10-12 15:00:57 +0300

    changes in the JS and CSS

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const content = document.querySelector('.grid-container');
+const searchBar = document.querySelector('#searchBar');
+const pokemonCount = document.querySelector('.pokemonCount p');
+const generation1Button = document.querySelector('#IgenerationButton');
+const generation2Button = document.querySelector('#IIgenerationButton');
+const generation3Button = document.querySelector('#IIIgenerationButton');
+const generation4Button = document.querySelector('#IVgenerationButton');
+const generation5Button = document.querySelector('#VgenerationButton');
+const generation6Button = document.querySelector('#VIgenerationButton');
+const generation7Button = document.querySelector('#VIIgenerationButton');
+const generation8Button = document.querySelector('#VIIIgenerationButton');
 
 let pokeData = [1, 2, 3, 4, 5];
-
-const fetchData = async () => {
-  await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+const fetchData = async (url) => {
+  await fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const fetches = data.results.map((item) => {
@@ -13,6 +22,8 @@ const fetchData = async () => {
             return {
               id: data.id,
               name: data.name,
+              weight: data.weight,
+              height: data.height,
               img: data.sprites.other['official-artwork'].front_default,
               types: data.types.map((type) => type.type.name),
             };
@@ -20,15 +31,16 @@ const fetchData = async () => {
       });
       Promise.all(fetches).then((res) => {
         pokeData = res;
-        pokeCards();
-        console.log(pokeData);
       });
     });
 };
 
 const pokeCards = () => {
-  const cards = pokeData
+  const filteredPokeData = pokeData.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchBar.value.toLowerCase())
+  );
 
+  const cards = filteredPokeData
     .map((pokemon) => {
       const typeIcons = pokemon.types
         .map((type) => {
@@ -36,22 +48,66 @@ const pokeCards = () => {
         })
         .join('');
       return ` <div class="grid-item" id="grid1">
-    <div class=id><p>${pokemon.id}</p> </div>
-          <span><img src="${pokemon.img}"<></span>
+      <div class=id><p>${pokemon.id}</p> </div>
+          <span><img src="${pokemon.img}" alt="${pokemon.name}"/></span>
           <div class="pokeInfo">
-            <h3>${pokemon.name}</h3></span>
+            <h3>${pokemon.name}</h3>
              <div class="types">
            ${typeIcons}
              </div>
-         <div class="typesNames">
-         <p>${pokemon.types}</p>
-         </div>
-         </div>
+            <div class="information">
+            <p>${pokemon.weight}KG | ${pokemon.height}CM</p>
+            </div>
+        </div>
       </div>`;
     })
     .join('');
 
   content.innerHTML = cards;
+  pokemonCount.textContent = `The number of Pokemon displayed are: ${filteredPokeData.length}`;
 };
 
-fetchData();
+searchBar.addEventListener('input', pokeCards);
+
+generation1Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(
+    pokeCards
+  );
+});
+
+generation2Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=100&offset=151').then(
+    pokeCards
+  );
+});
+
+generation3Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=135&offset=251').then(
+    pokeCards
+  );
+});
+generation4Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=107&offset=386').then(
+    pokeCards
+  );
+});
+generation5Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=156&offset=493').then(
+    pokeCards
+  );
+});
+generation6Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=72&offset=649').then(
+    pokeCards
+  );
+});
+generation7Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=88&offset=721').then(
+    pokeCards
+  );
+});
+generation8Button.addEventListener('click', () => {
+  fetchData('https://pokeapi.co/api/v2/pokemon?limit=96&offset=809').then(
+    pokeCards
+  );
+});
